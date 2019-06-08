8657e59ee03b80394bd4a91dda32ec7a96c2a08e
Angel Fernando <>
2019-06-07 19:46:20 -0500

    Se agrega servicio de creación de tutor

--- TutorService.php
+++ TutorService.php
@@ -16,6 +16,8 @@ class TutorService extends REST_Controller {
         $this->load->database();
     }
     
+//http://www.michael-pratt.com/blog/7/Encontrar-Lugares-cercanos-con-MySQL-y-PHP/
+
     public function ObtenerTutores_get()
     {
         /*$query = $this->db->query('SELECT * FROM tutor');
@@ -26,25 +28,69 @@ class TutorService extends REST_Controller {
             );
         
         $this->response($respuesta);*/
+
+        $idMateria = $this->uri->segment(3);
+        $distance = $this->uri->segment(4);
+        $lat = $this->uri->segment(5);
+        $lng = $this->uri->segment(6);
         
-        
-        $lat  =  4.6665578;
-        $lng =  -74.0524521;
-        $distance = 1; // Sitios que se encuentren en un radio de 1KM
+        //$lat  =  4.6665578;
+        //$lng =  -74.0524521;
+        //$distance = 1; // Sitios que se encuentren en un radio de 1KM
         $box = $this->getBoundaries($lat, $lng, $distance);
-        $query = $this->db->query('SELECT *, (6371 * ACOS( SIN(RADIANS(latitud)) 
-        * SIN(RADIANS(' . $lat . ')) + COS(RADIANS(longitud - ' . $lng . ')) 
-        * COS(RADIANS(latitud)) * COS(RADIANS(' . $lat . ')))) AS distance 
-        FROM tutor 
-        WHERE (latitud BETWEEN ' . $box['min_lat']. ' AND ' . $box['max_lat'] . ') 
-        AND (longitud BETWEEN ' . $box['min_lng']. ' AND ' . $box['max_lng']. ') 
-        HAVING distance  < ' . $distance . ' 
+        $query = $this->db->query('SELECT *, (6371 * ACOS( SIN(RADIANS(t.latitud)) 
+        * SIN(RADIANS(' . $lat . ')) + COS(RADIANS(t.longitud - ' . $lng . ')) 
+        * COS(RADIANS(t.latitud)) * COS(RADIANS(' . $lat . ')))) AS distance 
+        FROM tutor t INNER JOIN tutoresMaterias tm ON t.idTutor = tm.idTutor
+        WHERE (t.latitud BETWEEN ' . $box['min_lat']. ' AND ' . $box['max_lat'] . ') 
+        AND (t.longitud BETWEEN ' . $box['min_lng']. ' AND ' . $box['max_lng']. ') 
+        AND tm.idMateria = '. $idMateria .'
+        HAVING distance < ' . $distance . '
         ORDER BY distance ASC');
-        //$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         $respuesta = array(
                 'error' => FALSE,
                 'tutores' => $query->result_array()
             );
+        
+        $this->response($respuesta);
+    }
+
+    public function RegistrarTutor_post()
+    {
+        
+            $data = array(
+                'idTutor' => $this->input->post('telefono'),
+                'nombre' => $this->input->post('nombre'),
+                'direccion' => $this->input->post('direccion'),
+                'correo' => $this->input->post('correo'),
+                'telefono' => $this->input->post('telefono'),
+                'link' => $this->input->post('llink'),
+                'latitud' => null,
+                'longitud' => null,
+                'comentarios' => $this->input->post('comentarios'),
+                'fechaRegistro' => '2019',
+                'fechaUltimoPago' => null,            
+                'fechaProximoPago' => null,
+                'procesado' => false,
+                'activo' => false
+            );
+
+try{
+            
+            $this->db->insert('tutor', $data);
+
+            $respuesta = array(
+                'error' => FALSE,
+                'idinsertado' => $this->db->insert_id()
+            );
+
+        } catch (Exception $ex){
+            $respuesta = array(
+                'error' => TRUE,
+                'mensaje' => $ex->getMessage()
+            );
+        }
         
         $this->response($respuesta);
     }
